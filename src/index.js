3b9ed52db72f75c34b4f9686ad3a9a89660bdd48
omarupk33 <kkomar12334@gmail.com>
2025-09-16 14:21:12 +0300

    message

---
+++ src/index.js
@@ -0,0 +1,144 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// element.classList.add('hello');
+
+
+//  // Add the image to our existing div.
+//   const myIcon = new Image();
+//   myIcon.src = Icon;
+
+//   element.appendChild(myIcon);
+
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+
+// Header
+const header = document.querySelector('.header')
+
+
+const profile_block = document.createElement('div')
+      profile_block.className = 'profile_block'   
+      profile_block.style.height = '20px'
+      profile_block.style.wdith = '20px'
+      profile_block.textContent = 'Default Text'
+      profile_block.style.backgroundColor = 'green'
+
+
+
+const edting_options = document.createElement('div')
+      edting_options.className = 'editing_options'
+      edting_options.textContent = 'Default Text'
+      edting_options.style.height = '20px'
+      edting_options.style.wdith = '20px'
+      edting_options.style.backgroundColor = 'yellow'
+
+
+
+const logo_block = document.createElement('div')
+      logo_block.className = 'logo_block'
+      logo_block.textContent = 'Default Text'
+      logo_block.style.height = '20px'
+      logo_block.style.wdith = '20px'
+      logo_block.style.backgroundColor = 'purple'
+
+// appendning
+
+header.appendChild(profile_block)
+header.appendChild(edting_options)
+header.appendChild(logo_block)
+
+
+
+// content
+const content = document.querySelector('.content')
+
+const main_form = document.createElement('form') 
+main_form.className = 'main_form'
+
+
+//  Search Bar
+const search_bar = document.createElement('label')
+search_bar.style.height = '40px' 
+search_bar.style.width = '40px'
+search_bar.id = 'search_bar'
+search_bar.for = 'submit_search_bar'
+search_bar.textContent = 'Search: '
+search_bar.style.background = 'green'
+
+// search bar btn
+const submit_search_bar = document.createElement('input')
+submit_search_bar.style.height = '40px' 
+submit_search_bar.style.width = '400px'
+submit_search_bar.style.marginRight = '100px'
+submit_search_bar.type = 'text'
+submit_search_bar.id = 'submit_search_bar'
+submit_search_bar.style.background = 'blue'
+
+
+// A new task label
+const make_a_new_task = document.createElement('label')
+make_a_new_task.style.height = '40px' 
+make_a_new_task.style.width = '40px'
+make_a_new_task.id = 'new_task'
+make_a_new_task.for = 'new_task_btn'
+make_a_new_task.textContent = 'New Task: '
+make_a_new_task.style.background = 'azure'
+// Its button
+const new_task_btn = document.createElement('input')
+new_task_btn.style.height = '40px' 
+new_task_btn.style.width = '40px'
+new_task_btn.type = 'button'
+new_task_btn.id = 'new_task_btn'
+new_task_btn.style.background = 'azure'
+
+
+// Main list of tasks
+let to_do_list = document.createElement('ul')
+to_do_list.style.height = '500px' 
+to_do_list.style.width = '1000px'
+to_do_list.id = 'to_do_container'
+to_do_list.style.background = 'red'
+
+
+// appendning
+main_form.appendChild(search_bar)
+main_form.appendChild(submit_search_bar)
+main_form.appendChild(make_a_new_task)
+main_form.appendChild(new_task_btn)
+
+content.appendChild(main_form)
+content.appendChild(to_do_list)
+
+// footer
+const footer = document.querySelector("footer")
+const lisence = document.createElement('h1')
+lisence.id = 'lisence' 
+lisence.textContent = 'The Odin Project'
+
+// appendning
+footer.appendChild(lisence)
+
+// Functionality 
+
+// something is wrong and that's annoying!
+new_task_btn.addEventListener('click', ()=>{
+      const input_user = prompt('The task is called: ', 'task')
+      // const task_user = prompt("The task's content is: ", 'empty')
+      
+      let list_element = document.createElement('li')
+
+      list_element = `${input_user}`
+      // list_element.textContent = `${task_user}`
+
+      to_do_list.appendChild(list_element)
+})
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/templete.html"],
+  },
+  
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      filename: 'index.html',
+      title: 'Development',
+    }),
+
+  ],
+module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+
+    ],
+  },
+};
+
