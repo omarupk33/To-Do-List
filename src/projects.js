a7aa2838b493017f27b0263b57df39b0296cdd75
omarupk33 <kkomar12334@gmail.com>
2025-10-23 20:06:36 +0300

    message

--- src/projects.js
+++ src/projects.js
@@ -1,13 +1,21 @@
 export class Project{
     constructor(){
-// New project
-    const project_container = document.createElement('div')
-    project_container.id = 'project_content'
 
+    const project_container = document.createElement('div')
+    project_container.id = 'project_container'
+
+    let create_project = document.createElement('button')
+        create_project.id = 'make_project'
+        create_project.textContent = 'Make a new Project'
+
+    let create_base = document.createElement('div')
+        create_base.id = 'make_base'
+
+
+        project_container.appendChild(create_project)
+        project_container.appendChild(create_base)
 
     return project_container
     }
 
-    
-    
 }
