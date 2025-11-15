ad3f3679b64e8b7fb9d22c8e10f6c8602625f7f6
omarupk33 <kkomar12334@gmail.com>
2025-11-15 22:32:21 +0300

    message

--- src/files.js
+++ src/files.js
@@ -5,19 +5,18 @@ export function file_container(file_name){
 
       task_container()
 
-
-      // Should make something to open only the specific file
-      // Should push the task cards into the files
-
       let file_container = document.createElement('div')
       file_container.id = file_name
       file_container.style.border = 'solid 2px pink'
       file_container.style.height = '100px'
       file_container.style.width = '200px'
 
-      
+      // Last part
 
-      // file_container.appendChild()
+//       if (file_container.classList.contains('open')){
+//       let ordered_list = document.getElementById('to_do_container')
+//       file_container.appendChild(ordered_list)
+// }
 
       let container= document.getElementById('to_do_container')
       container.appendChild(file_container)
