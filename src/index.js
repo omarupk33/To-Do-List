57330e9ce5bab091256fb2fc2812ac31d91db049
omarupk33 <kkomar12334@gmail.com>
2025-11-16 23:02:31 +0300

    message

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ import { Project } from './projects.js'
 import { project_container } from './project_container'
 
 
-// content
 const content = document.querySelector('.content')
       content.id = 'content'
 
 function user_interface(){
 
 
-// footer
 const footer = document.querySelector("footer")
 const lisence = document.createElement('h1')
 lisence.id = 'lisence' 
 lisence.textContent = 'The Odin Project'
 
-// appending
 footer.appendChild(lisence)
 
 
